e06d2053aff76506589086f6dda49a78ebb026d7
Noah Perry <78505168+noah-perry@users.noreply.github.com>
2026-09-14 13:18:01 -0700

    Specifying crawl for reproducibility

--- scripts/01_process_data/process_commoncrawl.py
+++ scripts/01_process_data/process_commoncrawl.py
@@ -21,15 +21,11 @@ cc = pd.read_csv("00_data_raw/commoncrawl/languages.csv", keep_default_na = Fals
 
 
 # %% Clean data
-crawl_list = cc["crawl"].unique().tolist()
-last_crawl = max(crawl_list)
-    # find identifier of most recent crawl
+crawl_to_use = "CC-MAIN-2026-25"
+    # most recent crawl at time of analysis
 
-assert last_crawl == "CC-MAIN-2026-25"
-    # assertion fails if data for a more recent crawl has been added
-
-cc_last = cc.loc[cc["crawl"] == last_crawl,]
-    # filter data to most recent crawl
+cc_last = cc.loc[cc["crawl"] == crawl_to_use,]
+    # filter data to one crawl
 
 cc_last = cc_last.loc[cc_last["primary_language"] != "<unknown>",]
     # exclude data where language is <unknown>
